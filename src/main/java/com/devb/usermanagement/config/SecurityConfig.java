9c476dfa2ded56bd697533eb579b855dacdb805b
valter correia <baldhuino@gmail.com>
2023-06-11 22:13:05 +0100

    FilterChain

--- src/main/java/com/devb/usermanagement/config/SecurityConfig.java
+++ src/main/java/com/devb/usermanagement/config/SecurityConfig.java
@@ -1,5 +1,45 @@
 package com.devb.usermanagement.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+
+public class SecurityConfig {
+	
+	@SuppressWarnings("removal")
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		
+	    final JwtRequestFilter jwtRequestFilter = null;
+		final AuthenticationProvider authenticationProvider = null;
+		
+		
+		
+		http
+		.csrf().disable()
+		.authorizeHttpRequests()
+		.requestMatchers("")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.authenticationProvider(authenticationProvider)
+		.addFilterBefore(jwtRequestFilter,UsernamePasswordAuthenticationFilter.class);
+
+		
+		return http.build();
+	}
 }
