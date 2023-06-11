11269ef8d7aa130bd97fa686dc74d13cb3484c66
valter correia <baldhuino@gmail.com>
2023-06-11 15:39:03 +0100

    JwtService done

--- src/main/java/com/devb/usermanagement/config/JwtRequestFilter.java
+++ src/main/java/com/devb/usermanagement/config/JwtRequestFilter.java
@@ -2,22 +2,38 @@ package com.devb.usermanagement.config;
 
 import java.io.IOException;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.devb.usermanagement.service.JwtService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
+
+@Component
+@RequiredArgsConstructor
 public class JwtRequestFilter extends OncePerRequestFilter {
 
+	JwtService jwtService;
+	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, 
 			HttpServletResponse response, 
 			FilterChain filterChain)
 			throws ServletException, IOException {
-		
-		
+		final String authHeader = request.getHeader("Authorization");
+		final String jwt;
+		final String userEmail;
+		if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		jwt = authHeader.substring(7);
+		userEmail = jwtService.extractUserEmail(jwt); 
 	}
 
 }
